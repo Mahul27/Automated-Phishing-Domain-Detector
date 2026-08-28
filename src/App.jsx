46493a6027156429350fca52e9fb3e30c2f16792
Bhupinder22500650 <Bhupindersinghrakhra99@gmail.com>
2026-08-28 13:23:44 +1200

    implement shared layout component and standardize authenticated routes using react-router-dom Outlet

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Dashboard from './components/Dashboard';
 import ManualScan from './components/ManualScan';
 import ScanHistory from './components/ScanHistory';
 import ScanResult from './components/ScanResult';
+import Layout from './components/Layout';
 import './index.css';
 
 function App() {
@@ -11,10 +12,14 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/scan" element={<ManualScan />} />
-        <Route path="/history" element={<ScanHistory />} />
-        <Route path="/result" element={<ScanResult />} />
+        
+        {/* Authenticated Routes with Sidebar/Layout */}
+        <Route element={<Layout />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/scan" element={<ManualScan />} />
+          <Route path="/history" element={<ScanHistory />} />
+          <Route path="/result" element={<ScanResult />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
