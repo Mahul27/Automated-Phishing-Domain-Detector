5e841a7e2f9dc851623282e28b1f3a1b1be0cc15
Mahul <mahulpatel27@gmail.com>
2026-09-02 22:46:10 +1200

    Fixing the data split script so that it will eventually split the data and added comment to the line of codes

--- Train_Test_Split.py
+++ Train_Test_Split.py
@@ -1,15 +1,21 @@
 import pandas as pd 
 from sklearn.model_selection import train_test_split
 
-df = pd.read_csv('')  # Load the CSV file containing domain names and labels
+df = pd.read_csv('Training:Testing Dataset.csv')  # Load the CSV file containing domain names and labels
 
-feature_columns = ['domain_length', 'num_hyphens', 'entropy_domain', 'domain_age_days', 'expiration_days', 'is_suspicious_tld', 'num_digits', 'contains_brand_keyword', 'typosquat_similarity', 'ssl_cert_age_days']
+# Define the feature columns to be used for training the model
+#This list contains the names of the columns in the DataFrame that will be used as features for training the machine learning model. These features are selected based on their relevance to the task of detecting phishing domains.
+feature_columns = ['domain_length', 'num_hyphens', 'entropy_domain', 'domain_age_days', 'expiration_days', 'is_suspicious_tld', 'Num_Digits', 'Contains_Brand_Keyword', 'Typosquat_Similarity', 'SSL_Cert_Age_Days']
 
+# Split the dataset into features (X) and labels (y)
 x = df[feature_columns]  # Features
 y = df['label']  # Labels
 
-X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)  # Split the data into training and testing sets
+# Split the data into training and testing sets (80% training, 20% testing)
+#Using stratified sampling to ensure that the distribution of labels is similar in both the training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, stratify=y, random_state=42) 
 
+# Print the sizes of the training and testing sets, as well as the distribution of labels in each set
 print(f"Training set size:", X_train.shape)
 print(f"Testing set size:", X_test.shape)
 print(y_train.value_counts())
